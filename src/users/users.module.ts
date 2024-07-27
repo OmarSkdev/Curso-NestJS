b4b5399fadc44c50e104fa1061e6649865910c02
Omar <omar.sk80@gmail.com>
2024-07-27 01:54:46 -0400

    DTOs, Patron repositorio y Servicios

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersEntity } from './entities/users.entity';
+import { UsersService } from './service/users.service';
+import { UsersController } from './controllers/users.controller';
 
 
 @Module({
-  imports: [UsersModule],
-  
+  imports: [
+    TypeOrmModule.forFeature([UsersEntity])
+  ],
+  providers: [UsersService],
+  controllers: [UsersController]
 })
 export class UsersModule {}
