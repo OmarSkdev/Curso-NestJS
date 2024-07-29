63ae827abe20a382ebaf7b21632284104a617eb7
Omar <omar.sk80@gmail.com>
2024-07-29 15:49:54 -0400

    Error Handlers Controladores

--- src/projects/controllers/projects.controller.ts
+++ src/projects/controllers/projects.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { ProjectsService } from '../service/project.service';
-import { ProjectDTO } from '../dto/project.dto';
+import { ProjectDTO, ProjectUpdateDTO } from '../dto/project.dto';
 
 @Controller('projects')
 export class ProjectsController {
@@ -10,4 +10,20 @@ export class ProjectsController {
     public async createProject(@Body() body: ProjectDTO){
         return await this.projectService.createProject(body);
     }
+
+    @Get('all')
+    public async findAllProjects() {
+      return await this.projectService.findProjects();
+    }
+
+    @Get(':id')
+    public async findProjectById(@Param('id') id: string) {
+    return await this.projectService.findProjectById(id);
+    }
+
+    @Put('edit/:id')
+    public async updateProject(@Param('id') id:string, @Body() body: ProjectUpdateDTO) {
+        return await this.projectService.updateProject(body, id);
+    }
+  
 }
--- src/projects/service/project.service.ts
+++ src/projects/service/project.service.ts
@@ -1,8 +1,8 @@
 import { InjectRepository } from "@nestjs/typeorm";
 import { ProjectsEntity } from "../entities/projects.entity";
-import { Repository } from "typeorm";
+import { Repository, UpdateResult } from "typeorm";
 import { UsersEntity } from "src/users/entities/users.entity";
-import { ProjectDTO } from "../dto/project.dto";
+import { ProjectDTO, ProjectUpdateDTO } from "../dto/project.dto";
 import { Injectable } from "@nestjs/common";
 import { UsersProjectsEntity } from "src/users/entities/usersProjects.entity";
 import { UsersService } from "src/users/service/users.service";
@@ -24,5 +24,58 @@ export class ProjectsService {
         } catch (error) {
           throw ErrorManager.createSignatureError(error.message);
         }
+    }
+
+    public async findProjects(): Promise<ProjectsEntity[]> {
+      try {
+        const projects: ProjectsEntity[] = await this.projectRepository.find();
+        if (projects.length === 0) {
+          throw new ErrorManager({
+            type: 'BAD_REQUEST',
+            message: 'No se encontro resultado',
+          });
+        }
+        return projects;
+        
+      } catch (error) {
+        throw ErrorManager.createSignatureError(error.message);
+      }
+    }
+
+    public async findProjectById(id:string): Promise<ProjectsEntity>{
+      try {
+        const project = await this.projectRepository
+        .createQueryBuilder('project')
+        .where({ id })
+        .getOne();
+      if (!project) {
+        throw new ErrorManager({
+          type: 'BAD_REQUEST',
+          message: 'No existe proyecto con el id ' + id,
+        });
+      }
+      return project;
+        
+      } catch (error) {
+        throw ErrorManager.createSignatureError(error.message);
+      }
+    }
+
+    public async updateProject(body: ProjectUpdateDTO, id: string): Promise<UpdateResult | undefined> {
+      try {
+        const project: UpdateResult = await this.projectRepository.update(
+          id,
+          body,
+        );
+        if (project.affected === 0) {
+          throw new ErrorManager({
+            type: 'BAD_REQUEST',
+            message: 'No se pudo actualizar proyecto',
+          });
+        }
+        return project;
+      } catch (error) {
+        throw ErrorManager.createSignatureError(error.message);
+      }
     }
 }
